77b062019091b9eab14cc6e5ccd62f40a0642110
Rets <richcuizon2@gmail.com>
2025-12-05 00:51:41 +0800

    Setting Up

---
+++ src/main/GamePanel.java
@@ -0,0 +1,35 @@
+package src.main;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+    }
+}
